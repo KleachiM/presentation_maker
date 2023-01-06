f819cf234222fd3a850922d41c5ce5b071a3b826
Михаил Калинин <65865966+KleachiM@users.noreply.github.com>
2023-01-06 10:35:12 +0300

    DragAndDrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import './App.css';
-import {Presentation} from "./models/types";
-import {Miniatures} from "./components/miniatures/Miniatures";
-import {HeaderPanel} from "./components/header_panel/HeaderPanel";
-import {MainSlide} from "./components/mainslide/MainSlide";
-import {connect} from "react-redux";
+import Miniatures from "./Components/Miniatures/Miniatures";
+import {HeaderPanel} from "./Components/HeaderPanel/HeaderPanel";
+import MainSlide from "./Components/MainSlide/MainSlide";
 
-type AppProps = {
-  presentation: Presentation
-}
-
-function App(props: AppProps) {
+export default function App() {
   return (
     <div className="App">
         <div className="App-header">
-            <HeaderPanel/>
+            <HeaderPanel />
         </div>
         <div className="App-body">
-            <Miniatures presentation={props.presentation}/>
-            <MainSlide presentation={props.presentation}/>
+            <Miniatures />
+            <MainSlide />
         </div>
     </div>
   );
 }
-
-
-const mapStateToProps = (state: Presentation) => ({
-    presentation: state
-})
-
-export default connect(mapStateToProps)(App)
